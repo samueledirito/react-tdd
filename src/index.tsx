13759a8fc1e6af390ab1b5bc75657a118e5f7e62
Samuele Di Rito <samuele.dirito@toconsulting.it>
2020-04-23 20:02:39 +0200

    Added redux-saga and updated deps

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,15 @@ import { createHashHistory } from "history";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-// interface AppState extends DefaultRootState {
-//   isAuthenticated: boolean;
-//   profile: any;
-// }
-
 interface Action {
   type: string;
 }
 
 const INITIAL_STATE = { isAuthenticated: false, profile: null };
-const rootReducer = (state = INITIAL_STATE, action: Action) => state;
+const rootReducer = (state = INITIAL_STATE, action: Action) => {
+  if (!action.type) throw Error("action type not provided");
+  return state;
+};
 export type RootState = ReturnType<typeof rootReducer>;
 
 const history = createHashHistory();
